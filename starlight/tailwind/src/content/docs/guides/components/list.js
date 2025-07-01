bdda09fc9fe99d404845b3c59831b0f5b0ae5803
Brian Duong <dbiny3css@gmail.com>
2025-07-01 15:45:31 -0500

    feat(astro): Create grid component
    This grid component shows an overview of all of the components that
    exist in my component library, similar to shadcn/ui's components page.
    
    I created list.js to store all of my components in my component library,
    including their name and their respective mdx page location
    
    I can then import the array from list.js to ComponentGallery.tsx, so I can
    map through the array and render the contents of my grid component.

---
+++ starlight/tailwind/src/content/docs/guides/components/list.js
@@ -0,0 +1,10 @@
+export const components = [
+  { name: "One Column", pageLink: "one-column" },
+  { name: "Two Column", pageLink: "two-column" },
+  { name: "Three Column", pageLink: "three-column" },
+  { name: "Three Section", pageLink: "three-section" },
+  { name: "Holy Grail Layout", pageLink: "holy-grail" },
+  { name: "Full Section Hero", pageLink: "full-section-hero" },
+  { name: "Card Grid", pageLink: "card-grid" },
+  { name: "Bento Grid", pageLink: "bento-grid" },
+];
