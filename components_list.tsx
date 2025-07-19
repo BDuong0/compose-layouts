28a6c9a40ca09d0ff6948a71faf92000e17500be
Brian Duong <dbiny3css@gmail.com>
2025-07-19 13:54:21 -0500

    feat: Create components_list.tsx
    I wanted to create a file that contains overview information about each
    component in my component library so when I create my documentation, I
    just have to import 1 file and I have all of the information I need for
    each component.

---
+++ components_list.tsx
@@ -0,0 +1,40 @@
+type component = {
+  [key: string]: { mdxFilename: string; demoPath: string };
+};
+
+const components: component = {
+  "One Column": {
+    mdxFilename: "one-column",
+    demoPath: "src/components/demos/OneColumn/",
+  },
+  "Two Column": {
+    mdxFilename: "two-column",
+    demoPath: "src/components/demos/TwoColumn/",
+  },
+  "Three Column": {
+    mdxFilename: "three-column",
+    demoPath: "src/components/demos/ThreeColumn/",
+  },
+  "Three Section": {
+    mdxFilename: "three-section",
+    demoPath: "src/components/demos/ThreeSection/",
+  },
+  "Holy Grail": {
+    mdxFilename: "holy-grail",
+    demoPath: "src/components/demos/HolyGrail/",
+  },
+  "Full Screen Hero": {
+    mdxFilename: "full-screen-hero",
+    demoPath: "src/components/demos/FullScreenHero/",
+  },
+  "Card Grid": {
+    mdxFilename: "card-grid",
+    demoPath: "src/components/demos/CardGrid/",
+  },
+  "Bento Grid": {
+    mdxFilename: "bento-grid",
+    demoPath: "src/components/demos/BentoGrid/",
+  },
+};
+
+export default components;
