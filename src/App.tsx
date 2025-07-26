8afce2da29ca4379be5f8e36fe4c616b360ef7b8
Brian Duong <dbiny3css@gmail.com>
2025-07-26 14:34:34 -0500

    feat(src/App.tsx): Add Card Grid component
    This Card Grid component will show an overview of all of the components
    in my component library with each link opening up each of my components
    own demo page, which shows how each component works with realistic content.

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,20 @@
-import BentoGrid from "./components/BentoGrid";
+import CardGrid from "./components/CardGrid";
+import components from "../components_list";
 
 function App() {
   return (
     <main>
-      <BentoGrid numOfItems={7} layoutNum={2} gap="var(--spacing-6)">
-        <BentoGrid.ItemOne>
-          <div className="text-neutral-450 mx-9 text-4xl">
-            <p className="">Item 1</p>
-          </div>
-        </BentoGrid.ItemOne>
+      <CardGrid maxColumns={3} minItemPixelWidth="200px" gap="4px">
+        {Object.entries(components).map(([componentName, properties]) => {
+          const demoPath = properties["demoPath"];
 
-        <BentoGrid.ItemTwo>
-          <p>Item 2</p>
-        </BentoGrid.ItemTwo>
-
-        <BentoGrid.ItemThree>
-          <p>Item 3</p>
-        </BentoGrid.ItemThree>
-
-        <BentoGrid.ItemFour>
-          <p>Item 4</p>
-        </BentoGrid.ItemFour>
-
-        <BentoGrid.ItemFive>
-          <p>Item 5</p>
-        </BentoGrid.ItemFive>
-
-        <BentoGrid.ItemSix>
-          <p>Item 6</p>
-        </BentoGrid.ItemSix>
-
-        <BentoGrid.ItemSeven>
-          <p>Item 7</p>
-        </BentoGrid.ItemSeven>
-      </BentoGrid>
+          return (
+            <a href={demoPath} key={componentName}>
+              {componentName}
+            </a>
+          );
+        })}
+      </CardGrid>
     </main>
   );
 }
