c45c4b5cd8f3ee3af9534fa658f75a1c17617502
Brian Duong <dbiny3css@gmail.com>
2025-08-04 15:58:27 -0500

    feat(demos/TwoColumn): Add content to TwoColumnDemo.tsx

--- src/components/demos/TwoColumn/TwoColumnDemo.tsx
+++ src/components/demos/TwoColumn/TwoColumnDemo.tsx
@@ -2,37 +2,55 @@ import TwoColumnLayout from "../../TwoColumnLayout";
 
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "../../../index.css";
-import "../../../index2.css";
+import "../../../page_setup.js";
+
+import { Button } from "@/components/ui/button";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <TwoColumnLayout layoutLevel="root">
-      <TwoColumnLayout.LeftColumn columnPercent={20} className="bg-red-400">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique,
-          neque nihil. Omnis est enim, tenetur voluptate sed totam et nihil ab
-          porro quam eveniet doloribus at eum expedita culpa vel! Aut optio
-          accusamus dolorem, enim perspiciatis id et voluptate assumenda
-          voluptatem ullam sequi esse error non quasi quaerat corrupti hic
-          quibusdam sed fugiat? Error tempore, nulla illo dignissimos sed
-          veniam.
-        </p>
-      </TwoColumnLayout.LeftColumn>
-      <TwoColumnLayout.RightColumn
-        columnPercent={80}
-        minWidth={"65ch"}
-        className="bg-blue-300"
+      <TwoColumnLayout.LeftColumn
+        columnPercent={50}
+        minWidth="515px"
+        className="grid h-dvh place-items-center *:col-span-full *:row-span-full"
       >
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, sed?
-          Sed, aut! Ipsam expedita perferendis et itaque necessitatibus libero
-          veniam inventore vel rem esse, dicta perspiciatis unde dolore
-          consectetur quasi. Adipisci quo rerum, natus dolorem aspernatur ab
-          voluptate deserunt. Veniam maiores ducimus id soluta officiis, impedit
-          qui facere eos! Libero, iusto cum optio ipsum itaque quibusdam quis
-          quaerat voluptates culpa.
-        </p>
+        <img
+          className="h-full object-cover"
+          src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.curioscene.com%2Fwp-content%2Fuploads%2F2020%2F06%2Fcreate-essential-graphics-with-pr-ae-9-1024x576.jpg&f=1&nofb=1&ipt=73e173e7d391902e79f16965ec15d7f3960ab7bd0ff7266f18bd045b7990e537"
+          alt=""
+        />
+        <div className="h-full w-full bg-stone-950/40"></div>
+        <div className="flex flex-col items-center">
+          <h1 className="mt-[0] text-center">After Effects for AMV 101</h1>
+          <p className="font-accent font-bold">
+            Introduction to Making Anime Music Videos in After Effects
+          </p>
+          <Button className="bg-stone-500 hover:bg-stone-600" asChild>
+            <a href="">Learn More</a>
+          </Button>
+        </div>
+      </TwoColumnLayout.LeftColumn>
+
+      <TwoColumnLayout.RightColumn
+        columnPercent={50}
+        minWidth="515px"
+        className="grid h-dvh place-items-center *:col-span-full *:row-span-full"
+      >
+        <img
+          className="h-full object-cover"
+          src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmedia.sketchfab.com%2Fmodels%2F0bfd0e2b49a348a4b64b20cc8196e3b3%2Fthumbnails%2Fe98eceb3eefb4aab9683f9aa71586913%2F7217f161fb684115b577d42d937622ce.jpeg&f=1&nofb=1&ipt=c9a22cece4abc30665dfdc7c2c34d6b84c60db94123d7d807bde41f0e01fd869"
+          alt=""
+        />
+        <div className="h-full w-full bg-stone-950/40"></div>
+        <div className="flex flex-col items-center">
+          <h1 className="mt-[0] text-center">Human Modeling in Blender 101</h1>
+          <p className="font-accent font-bold">
+            Introduction to Modeling Humans in Blender
+          </p>
+          <Button className="bg-stone-500 hover:bg-stone-600" asChild>
+            <a href="">Learn More</a>
+          </Button>
+        </div>
       </TwoColumnLayout.RightColumn>
     </TwoColumnLayout>
   </StrictMode>,
