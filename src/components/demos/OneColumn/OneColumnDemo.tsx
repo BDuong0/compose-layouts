5b9c047336893a6794784b0ef34f76a439e35a59
Brian Duong <dbiny3css@gmail.com>
2025-08-04 15:54:44 -0500

    feat(demos/OneColumn): Add content to OneColumnDemo.tsx

--- src/components/demos/OneColumn/OneColumnDemo.tsx
+++ src/components/demos/OneColumn/OneColumnDemo.tsx
@@ -2,12 +2,11 @@ import OneColumnLayout from "../../OneColumnLayout";
 
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "../../../index.css";
-import "../../../index2.css";
+import "../../../page_setup.js";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <OneColumnLayout>
+    <OneColumnLayout maxWidth="60ch">
       <h1>What I wish I knew before starting college</h1>
 
       <h2>Introduction</h2>
@@ -29,6 +28,13 @@ createRoot(document.getElementById("root")!).render(
       </p>
 
       <h2>Parking can be a pain in the ass</h2>
+      <figure className="flex flex-col justify-center items-center">
+        <img className="h-[240px]" src="https://www.stthomas.edu/_media-library/_images/campus-safety/stthomas-parking-transportation-lot.jpg" alt="Outdoor Parking lot" />
+        <figcaption className="mx-auto">
+          <a href="https://www.stthomas.edu/_media-library/_images/campus-safety/stthomas-parking-transportation-lot.jpg">Image from the University of St. Thomas in Minnesota</a>
+        </figcaption>
+      </figure>
+      
       <p>
         If you're going to live on campus for your entire college experience you
         can disregard this piece of advice. If you're living off campus and you
@@ -37,14 +43,14 @@ createRoot(document.getElementById("root")!).render(
         depending on the time of week and time of day you drive to your college
         campus, a different amount of opened parking spots are avaliable.
         Parking spots closer to a specific campus building are valueable and
-        thus these will. In a parking garage, usually all of the parking spots
+        these spots can be filled very fast. In a parking garage, usually all of the parking spots
         on the first floor are all filled, because no one likes walking a shit
-        ton
+        ton to their class.
       </p>
-
-      <ul>
+      
+      <ul className="list-disc">
         <li>
-          Morning [~6 AM - ~9 AM]: 50/50 change that you go to any parking lot
+          Morning [~6 AM - ~9 AM]: 50/50 chance that you go to any parking lot
           on your campus and there are opened spots. For parking garages,
           usualyl the first floor isn't filled up
         </li>
@@ -54,22 +60,28 @@ createRoot(document.getElementById("root")!).render(
         </li>
       </ul>
 
-      <p>Shortcut to know if a parking lot is completely full</p>
-      <p>I</p>
+      <h3>Shortcut to know if a parking lot is completely full</h3>
+      <p>When you drive around a parking lot, if you see more than one car exiting the parking lot, then that parking lot must be full, because no one likes to spend lots of time driving around in a parking lot.</p>
 
       <h2>
         Dealing with the hot days as you go to college your fall semesters
       </h2>
       <p>
         Your fall semester would usually be from August all the way to November
-        or December and the heat goes crazy in August and September.
+        or December and the heat goes crazy in August and September. You're driving to campus and on the way your head and back are sweating like crazy and now you have to go to class sweaty. Here's how you prevent that
+        terrible situation from happening.
       </p>
 
-      <p>A fan to cool yourself off</p>
-      <p>
-        Bring towels with you so you can wipe off the sweat off your torso,
-        especially your back
-      </p>
+      <h3>Bring a fan to cool yourself off</h3>
+      <figure className="flex flex-col justify-center items-center">
+        <img className="h-[240px]" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmedia.printables.com%2Fmedia%2Fprints%2F609926%2Fimages%2F6973189_1c055628-31cb-45d7-a538-e7ff67e6d868_f66bd8a6-beb5-4ef9-ae3a-56f099aa88ee%2Fimg-3.jpg&f=1&nofb=1&ipt=bd36663e812dd8ec5dafa129c86daeb91fe539708725fda887999626232f121c" alt="A handheld fan is standing up on a table with a smartphone leaning against it" />
+      </figure>
+      <p>If you do sweat a lot and it's unfessible for you to take a shower, because you're on campus then just hold fan and blow cool air on wherever you're sweating so you can cool off faster.</p>
+      <h3>Bring towels with you to wipe off sweat</h3>
+      <figure className="flex flex-col justify-center items-center">
+        <img className="h-[240px]" src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fpngimg.com%2Fuploads%2Ftowel%2Ftowel_PNG5.png&f=1&nofb=1&ipt=507fb9ff0a65151316ab7d4bd39e8e9cd6f5df72d5c9be378d9a9797bb368359" alt="A neatly folded up orange towel" />
+      </figure>
+      <p>When you're driving in a car in hot weather, your head and back may sweat a lot, so having a towel on hand to wipe off the sweat is useful.</p>
     </OneColumnLayout>
   </StrictMode>,
 );
