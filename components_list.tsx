c5184d0e37493321c73955f5889f0c2f755bf780
Brian Duong <dbiny3css@gmail.com>
2026-01-07 12:44:30 -0600

    refacotr(components_list): Change all demoPath
    All of the demoPath values need to change to reflect the new location of
    my component demo files. I need to find a more efficient way to set the
    value of the demoPath's, rather than manually editing the values myself,
    something similar to the path aliases I've set up in this Vite project.

--- components_list.tsx
+++ components_list.tsx
@@ -5,39 +5,39 @@ type component = {
 const components: component = {
   "One Column": {
     mdxFilename: "one-column",
-    demoPath: "src/components/demos/OneColumn/",
+    demoPath: "demo-lib/OneColumn/",
   },
   "Two Column": {
     mdxFilename: "two-column",
-    demoPath: "src/components/demos/TwoColumn/",
+    demoPath: "demo-lib/TwoColumn/",
   },
   "Three Column": {
     mdxFilename: "three-column",
-    demoPath: "src/components/demos/ThreeColumn/",
+    demoPath: "demo-lib/ThreeColumn/",
   },
   "Three Section": {
     mdxFilename: "three-section",
-    demoPath: "src/components/demos/ThreeSection/",
+    demoPath: "demo-lib/ThreeSection/",
   },
   "Holy Grail": {
     mdxFilename: "holy-grail",
-    demoPath: "src/components/demos/HolyGrail/",
+    demoPath: "demo-lib/HolyGrail/",
   },
   "Full Screen Hero": {
     mdxFilename: "full-screen-hero",
-    demoPath: "src/components/demos/FullScreenHero/",
+    demoPath: "demo-lib/FullScreenHero/",
   },
   "Card Grid": {
     mdxFilename: "card-grid",
-    demoPath: "src/components/demos/CardGrid/",
+    demoPath: "demo-lib/CardGrid/",
   },
   "Bento Grid": {
     mdxFilename: "bento-grid",
-    demoPath: "src/components/demos/BentoGrid/",
+    demoPath: "demo-lib/BentoGrid/",
   },
   "NavBar": {
     mdxFilename: "simple-navbar",
-    demoPath: "src/components/demos/NavBar/"
+    demoPath: "demo-lib/NavBar/"
   }
 };
 
